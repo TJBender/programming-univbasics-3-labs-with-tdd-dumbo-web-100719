7a53b0e628bc10add09333d57e3ffe4700df597d
Tiana Bender <tiana.bender01@gmail.com>
2019-09-17 02:14:27 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,7 @@
 first_number = 19
 second_number = 7
 sum = first_number + second_number
-
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
 
